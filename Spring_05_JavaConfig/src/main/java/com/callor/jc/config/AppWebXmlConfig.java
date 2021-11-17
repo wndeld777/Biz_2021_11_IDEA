f14bf3d264b4b7df5f8024b7596e4a7b764388a5
wndeld777 <wndeld777@gmail.com>
2021-11-17 17:46:14 +0900

    appWebxmlConfig

---
+++ Spring_05_JavaConfig/src/main/java/com/callor/jc/config/AppWebXmlConfig.java
@@ -0,0 +1,31 @@
+package com.callor.jc.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ * web.xml 을 대신할 config class
+ */
+public class AppWebXmlConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    // <context-param> 에 설정한 root-context.xml 을 초기화 할 method
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{RootConfig.class};
+    }
+
+    // <servlet> 에 설정한 servlet-context.xml 을 초기화 할 method
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{
+                ServletConfig.class,
+                MyBatisConfig.class
+        };
+    }
+
+    // servlet-mapping 에 설정한 url-pattern 을 초기화 할 method
+    @Override
+    protected String[] getServletMappings() {
+        String[] mapping = new String[]{"/"};
+        return mapping;
+    }
+}
